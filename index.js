a27936fddcb1ca6d28bc84760cf29da224ee63f1
Ian Kimani <ian.kiarie2002@gmail.com>
2024-05-29 07:12:10 +0300

    create count program
     We can help each other Style this.

---
+++ index.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function(){
+    count -= 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("resetBtn").onclick = function(){
+    count = 0;
+    document.getElementById("countLabel").innerHTML = count;
+}
+
+document.getElementById("increaseBtn").onclick = function(){
+    count += 1;
+    document.getElementById("countLabel").innerHTML = count;
+}
